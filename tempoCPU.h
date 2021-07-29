a9262956e01268da4e03530365ed545b3d7d9766
mlziade <ziade.michel@hotmail.com>
2021-07-29 16:38:08 -0300

    adicionado uso tempoCPU

--- tempoCPU.h
+++ tempoCPU.h
@@ -2,3 +2,16 @@
 #include <stdlib.h>
 
 void Tempo_CPU_Sistema(double *seg_CPU_total, double *seg_sistema_total);
+
+/*
+
+USO:
+  Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
+
+  **Chamada ao algoritmo a ser avaliado;**
+
+  Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
+
+  printf ("Tempo de CPU total = %f\n", s_CPU_final - s_CPU_inicial);
+  
+*/

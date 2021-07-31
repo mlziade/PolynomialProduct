7c18c37c466c82a51e6e3a9d8d38061bca883dcb
mlziade <ziade.michel@hotmail.com>
2021-07-30 21:30:06 -0300

    Divide and Conquer - bugado

--- main.c
+++ main.c
@@ -32,12 +32,36 @@ int* polynomialProductBruteForce(int s, int* a1, int* a2){
 
 int* polynomialProductDivideConquer(int n, int* a, int*b){
   
-  int* reslt;
+  int *reslt;
   reslt = (int*) malloc(sizeof(int) * ((n*2)-1) );
+  int *aux1, *aux2;
+  aux1 = (int*) malloc(sizeof(int) * (n-1));
+  aux2 = (int*) malloc(sizeof(int) * (n-1));
+
   int i,j;
 
   if (n > 2){ //Enquanto s for maior que 2
     //Algoritimo
+    
+    //A0B0
+    aux1 = polynomialProductDivideConquer(n/2, a, b);
+    for (i = 0; i < n-1; i++){
+      reslt[i] = aux1[i];
+    }
+
+    //A0B1
+    aux1 = polynomialProductDivideConquer(n/2, a, b + sizeof(int) * n/2);
+    aux2 = polynomialProductDivideConquer(n/2, a + sizeof(int) * n/2, b);
+    for (i = n/2 ;i < n-1; i++){
+      reslt[i] = aux1[i] + aux2[i];
+    }
+
+    //A1B1
+    aux1 = polynomialProductDivideConquer(n/2, a + sizeof(int) * n/2, b + sizeof(int) * n/2);
+    for (i = n; i < n*2 -1; i++){
+      reslt[i] = aux1[i];
+    }
+    return reslt;
   }
   else{ //Quando s chegar a 2
     reslt[0] = (a[0]*b[0]);
@@ -45,9 +69,7 @@ int* polynomialProductDivideConquer(int n, int* a, int*b){
     reslt[2] = (a[1]*b[1]);
     return reslt;
   }
-  
-  
-
+  return reslt;
 }
 
 int main(){
@@ -58,7 +80,8 @@ int main(){
   int* arrayBF = polynomialProductBruteForce(size, array1, array2); 
   printPolynom(size * 2, arrayBF);
 
-  // int* arrayDC = polynomialProductDivideConquer(size1, array1, size2, array2);
+  int* arrayDC = polynomialProductDivideConquer(size, array1, array2);
+  printPolynom(size * 2, arrayDC);
 
   return 0;
 }

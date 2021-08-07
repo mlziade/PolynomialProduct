71fcbfcc76f33696d07aee1b9617b6a29df5cc29
Pedro Bandoli <pedrobandoli@id.uff.br>
2021-08-06 23:50:59 -0300

    wip: Modularização e refatoração do código

---
+++ produtoPolinomial.h
@@ -0,0 +1,6 @@
+void calculaPol(int size, int* array1, int* array2, FILE** outputs);
+int* polynomialProductBruteForce(int s,  int* a1, int* a2);
+int* polynomialProductuDivideConquer4(int n,  int* a, int* b);
+int* polynomialProductuDivideConquer3(int n,  int* a, int* b);
+int* polynomialSumMinus(int n, int* a, int* b, int operacao);
+void printPolynom(int s, int*a);
--- tempoCPU.h
+++ tempoCPU.h
@@ -1,17 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Tempo_CPU_Sistema(double *seg_CPU_total, double *seg_sistema_total);
-
-/*
-
-USO:
-  Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
-
-  **Chamada ao algoritmo a ser avaliado;**
-
-  Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
-
-  printf ("Tempo de CPU total = %f\n", s_CPU_final - s_CPU_inicial);
-  
-*/
+void Tempo_CPU_Sistema(double *seg_CPU_total);
